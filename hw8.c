b4d9a5d1c64ae5a162693c9314319ea31177ef4a
limlimseoyn <113499645+limlimseoyn@users.noreply.github.com>
2022-11-22 15:38:27 +0900

    hw8.c

---
+++ hw8.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+double pow(double a, double b);
+double sqrt(double x);
+
+int main(void){
+	arrayR[5] = {};
+	double result = 0;
+	
+	printf("Enter 5 real numbers: ");
+	scanf("%d %d %d %d %d", &arrayN[0], &arrayN[1], &arrayN[2], &arrayN[3], &arrayN[4]);
+	
+	printf("Standard Deviation = %.3f", result);
+	
+	return 0;
+}
+
+double pow(double a, double b){
+	
+	return 0;
+}
+
+double sqrt(double x){
+	
+	return 0;
+}
